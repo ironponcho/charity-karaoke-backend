52886ea4a7f56fbbc978491b77435404d0fae1ae
Matthias <osthues.matthias@gmail.com>
2020-09-10 16:16:31 +0200

    add KaraokeController.kt with create, getAll and getById path

---
+++ src/main/kotlin/de/charitykaraoke/backend/controller/KaraokeController.kt
@@ -0,0 +1,25 @@
+package de.charitykaraoke.backend.controller
+
+import de.charitykaraoke.backend.entity.karaoke.Karaoke
+import de.charitykaraoke.backend.entity.karaoke.KaraokeRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
+
+
+@RestController
+@RequestMapping("/karaoke")
+class KaraokeController(@Autowired private val karaokeRepository: KaraokeRepository) {
+
+    @PostMapping()
+    fun createKaraoke(@RequestBody karaoke: Karaoke): Karaoke = karaokeRepository.save(karaoke)
+
+    @GetMapping("/{karaokeId}")
+    fun getKaraokeById(@PathVariable karaokeId: Int): ResponseEntity<Karaoke> =
+            karaokeRepository.findById(karaokeId).map {
+                ResponseEntity.ok(it)
+            }.orElse(ResponseEntity.notFound().build())
+
+    @GetMapping()
+    fun getAllJournals(): List<Karaoke> = karaokeRepository.findAll()
+}
