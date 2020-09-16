6e858748912749e3d5d7cb01acab418c1f9f38ca
Matthias <osthues.matthias@gmail.com>
2020-09-16 12:55:31 +0200

    import and code optimization for Karaoke.kt and KaraokeRepository.kt

--- src/main/kotlin/de/charitykaraoke/backend/entity/karaoke/Karaoke.kt
+++ src/main/kotlin/de/charitykaraoke/backend/entity/karaoke/Karaoke.kt
@@ -1,16 +1,17 @@
 package de.charitykaraoke.backend.entity.karaoke
 
-import javax.persistence.Entity
-import javax.persistence.GeneratedValue
-import javax.persistence.GenerationType
-import javax.persistence.Id
+import de.charitykaraoke.backend.entity.attendee.Attendee
+import javax.persistence.*
 import javax.validation.constraints.NotBlank
 
 @Entity(name = "karaokes")
 data class Karaoke(
         @Id @GeneratedValue(
                 strategy = GenerationType.IDENTITY) val id: Int = 0,
-        @get: NotBlank val name: String = "",
-        val expired: Boolean = false
+        @get: NotBlank var name: String,
+        var expired: Boolean = false,
+        @OneToMany(mappedBy = "karaoke", fetch = FetchType.LAZY,
+                cascade = [CascadeType.ALL])
+        var attendees: List<Attendee> = listOf()
 ) {
 }
--- src/main/kotlin/de/charitykaraoke/backend/entity/karaoke/KaraokeRepository.kt
+++ src/main/kotlin/de/charitykaraoke/backend/entity/karaoke/KaraokeRepository.kt
@@ -3,6 +3,8 @@ package de.charitykaraoke.backend.entity.karaoke
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
+
 @Repository
 interface KaraokeRepository : JpaRepository<Karaoke, Int> {
+
 }
